179a7d0db0182e063f8b3479081dcf03fb5dc536
marktiwnzhao <211250109@smail.nju.edu.cn>
2023-02-28 17:27:22 +0800

    lab0

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,34 @@
-public class Main
-{    
-    public static void main(String[] args){}
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class Main {
+    public static void main(String[] args) {
+        InputStream input = null;
+        try {
+            input = new FileInputStream(args[0]);
+            try {
+                byte[] bytes = new byte[input.available()];
+                int len;
+                String s;
+                while((len = input.read(bytes)) != -1) {
+                    s = new String(bytes, 0, len);
+                    System.out.print(s);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } finally {
+            if(input != null) {
+                try {
+                    input.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
 }
