d9a6b9246867c583a2be7e984877110660226410
marktiwnzhao <211250109@smail.nju.edu.cn>
2023-03-09 01:12:34 +0800

    lab1

--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,45 @@
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.Token;
+
 import java.io.IOException;
-import java.io.InputStream;
+import java.util.List;
+
 
 public class Main {
-    public static void main(String[] args) {
-        InputStream input = null;
-        try {
-            input = new FileInputStream(args[0]);
-            try {
-                byte[] bytes = new byte[input.available()];
-                int len;
-                String s;
-                while((len = input.read(bytes)) != -1) {
-                    s = new String(bytes, 0, len);
-                    System.out.print(s);
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } finally {
-            if(input != null) {
-                try {
-                    input.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
+    public static void main(String[] args) throws IOException {
+        if (args.length < 1) {
+            System.err.println("input path is required");
+        }
+        String source = args[0];
+        CharStream input = CharStreams.fromFileName(source);
+        SysYLexer sysYLexer = new SysYLexer(input);
+        sysYLexer.removeErrorListeners();
+        ErrorListener errorListener = new ErrorListener();
+        sysYLexer.addErrorListener(errorListener);
+        List<? extends Token> allTokens = sysYLexer.getAllTokens();
+
+        if (!errorListener.isError) {
+            //没有词法错误
+            String[] ruleNames = sysYLexer.getRuleNames();
+            for (Token token : allTokens) {
+                String ruleName = ruleNames[token.getType() - 1];
+                if (ruleName.equals("INTEGER_CONST")) {
+                    int num = parseInt(token.getText());
+                    System.err.println(ruleName + " " + num + " at Line " + token.getLine() + ".");
+                } else {
+                    System.err.println(ruleName + " " + token.getText() + " at Line " + token.getLine() + ".");
                 }
             }
         }
+    }
+
+    private static int parseInt(String text) {
+        if (text.startsWith("0x") || text.startsWith("0X")) {
+            return Integer.parseInt(text.substring(2), 16);
+        } else if (text.startsWith("0") && text.length() > 1) {
+            return Integer.parseInt(text.substring(1), 8);
+        }
+        return Integer.parseInt(text);
     }
 }
