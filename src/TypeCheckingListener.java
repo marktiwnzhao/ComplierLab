7f1ed1017120179b19cc8d2c3a2c33b2481c58f8
marktiwnzhao <211250109@smail.nju.edu.cn>
2023-04-22 22:51:04 +0800

    lab3

--- src/TypeCheckingListener.java
+++ src/TypeCheckingListener.java
@@ -65,7 +65,7 @@ public class TypeCheckingListener extends SysYParserBaseListener {
             } else {
                 List<TerminalNode> lBrackt = ctx.funcFParam(i).L_BRACKT();
                 Type type = (Type) globalScope.resolve(typeName);
-                if(lBrackt != null && lBrackt.size() == 1) {
+                if(lBrackt != null) {
                     type = new ArrayType(type, 1);// 一维数组（测试只会有一维数组）
                 }
                 parameterTypes.add(type);
